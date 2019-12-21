ef5840b3e3ac2ba987e5470dce3d220ec1e137b1
Vyacheslav Nikolaev <honeydrums@icloud.com>
2019-12-21 22:54:03 +0300

    Появление меню
    Добавил и анимировал появление/скрытие меню при скролле на мобильной версии

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var toggle = document.querySelector(".burger");
 var mMenu = document.querySelector(".main-nav-list");
+var fixed = document.querySelector(".main-nav");
 var close = document.querySelector(".close-menu");
 var toggleClose = document.querySelector(".burger")
 
@@ -13,3 +14,17 @@ close.addEventListener("click", function () {
 	mMenu.classList.toggle("main-nav-list__mobile");
 	toggle.classList.toggle("burger--close");
 });
+
+// window.addEventListener('scroll', function() {
+  
+// });
+
+window.addEventListener("wheel", function (evt) {
+    if (event.deltaY > 0) {
+        fixed.classList.remove("main-nav__fixed");
+        fixed.classList.remove("in-down");
+    } else {
+        fixed.classList.add("main-nav__fixed");
+        fixed.classList.add("in-down");
+    }
+}, false);
