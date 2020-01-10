53f88d16f6628ae51db6f7c193f07ab3917beffb
Vyacheslav Nikolaev <honeydrums@icloud.com>
2020-01-10 15:43:33 +0300

    Обновил стили, js
    Десктоп: поменял размер шрифта в футере.
    Мобилка: убрал outline с бургера, анимировал мобильное меню: появление и исчезновение.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
-var toggle = document.querySelector(".burger");
-var mMenu = document.querySelector(".main-nav-list");
+var burger = document.querySelector(".burger");
+var navMenu = document.querySelector(".main-nav-list");
 var fixed = document.querySelector(".main-header");
 var close = document.querySelector(".close-menu");
-var toggleClose = document.querySelector(".burger")
+var toggleClose = document.querySelector(".burger");
+var gallery = document.querySelector(".gallery-list");
+var about = document.querySelector(".about-list");
+var summer = document.querySelector(".summer-list");
+var button = document.querySelector(".button-list")
 
-toggle.addEventListener("click", function (evt) {
-	mMenu.classList.toggle("main-nav-list__mobile");
-	toggle.classList.toggle("change");
+burger.addEventListener("click", function (evt) {
+	navMenu.classList.toggle("main-nav-list__mobile");
+	burger.classList.toggle("change");
 	toggleClose.classList.toggle("toggle-button--close");
+	gallery.classList.toggle("li-show");
+	about.classList.toggle("li-show");
+	summer.classList.toggle("li-show");
+	button.classList.toggle("li-show");
 });
 
-close.addEventListener("click", function () {
-	mMenu.classList.toggle("main-nav-list__mobile");
-	toggle.classList.toggle("change");
+close.addEventListener("click", function (evt) {
+	navMenu.classList.toggle("main-nav-list__mobile");
+	burger.classList.toggle("change");
+	gallery.classList.toggle("li-show");
+	about.classList.toggle("li-show");
+	summer.classList.toggle("li-show");
+	button.classList.toggle("li-show");
 });
 
 window.addEventListener("scroll", function(evt) {
   if (this.oldScroll > this.scrollY) {
-    fixed.classList.add("main-nav__fixed");
-    fixed.classList.add("in-down");
+	fixed.classList.remove("main-header__hidden");
+	fixed.classList.add("main-header__fixed");
+	burger.classList.remove("change");
+	gallery.classList.remove("li-show");
+	about.classList.remove("li-show");
+	summer.classList.remove("li-show");
+	button.classList.remove("li-show");
   } else {
-    fixed.classList.remove("main-nav__fixed");
-    fixed.classList.remove("in-down");
-    mMenu.classList.remove("main-nav-list__mobile");
-    toggle.classList.remove("change");
+	fixed.classList.add("main-header__hidden");
+	fixed.classList.remove("main-header__fixed");
+	navMenu.classList.remove("main-nav-list__mobile");
+	burger.classList.remove("change");
+	gallery.classList.remove("li-show");
+	about.classList.remove("li-show");
+	summer.classList.remove("li-show");
+	button.classList.remove("li-show");
   }
   this.oldScroll = this.scrollY;
 }, false);
