8c387e3f4bfb7c03345b18169d5a56810b4267b3
Vyacheslav Nikolaev <honeydrums@icloud.com>
2020-01-24 01:25:12 +0300

    update styles
    fixed class names according to BEM methodology

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
 var burger = document.querySelector(".burger");
-var navMenu = document.querySelector(".main-nav-list");
+var navMenu = document.querySelector(".main-nav__list");
 var fixed = document.querySelector(".main-header");
 var close = document.querySelector(".close-menu");
 var toggleClose = document.querySelector(".burger");
-var gallery = document.querySelector(".gallery-list");
-var about = document.querySelector(".about-list");
-var summer = document.querySelector(".summer-list");
-var button = document.querySelector(".button-list")
+var gallery = document.querySelector(".main-nav__gallery");
+var about = document.querySelector(".main-nav__about");
+var summer = document.querySelector(".main-nav__summer");
+var button = document.querySelector(".main-nav__button")
 
 burger.addEventListener("click", function (evt) {
-	navMenu.classList.toggle("main-nav-list__mobile");
-	burger.classList.toggle("change");
+	navMenu.classList.toggle("main-nav__list_mobile");
+	burger.classList.toggle("burger_change");
 	toggleClose.classList.toggle("toggle-button--close");
-	gallery.classList.toggle("li-show");
-	about.classList.toggle("li-show");
-	summer.classList.toggle("li-show");
-	button.classList.toggle("li-show");
+	gallery.classList.toggle("main-nav__item_show");
+	about.classList.toggle("main-nav__item_show");
+	summer.classList.toggle("main-nav__item_show");
+	button.classList.toggle("main-nav__item_show");
 });
 
 close.addEventListener("click", function (evt) {
-	navMenu.classList.toggle("main-nav-list__mobile");
-	burger.classList.toggle("change");
-	gallery.classList.toggle("li-show");
-	about.classList.toggle("li-show");
-	summer.classList.toggle("li-show");
-	button.classList.toggle("li-show");
+	navMenu.classList.toggle("main-nav__list_mobile");
+	burger.classList.toggle("burger_change");
+	gallery.classList.toggle("main-nav__item_show");
+	about.classList.toggle("main-nav__item_show");
+	summer.classList.toggle("main-nav__item_show");
+	button.classList.toggle("main-nav__item_show");
 });
 
 window.addEventListener("scroll", function(evt) {
   if (this.oldScroll > this.scrollY) {
 	fixed.classList.remove("main-header__hidden");
 	fixed.classList.add("main-header__fixed");
-	burger.classList.remove("change");
-	gallery.classList.remove("li-show");
-	about.classList.remove("li-show");
-	summer.classList.remove("li-show");
-	button.classList.remove("li-show");
+	burger.classList.remove("burger_change");
+	gallery.classList.remove("main-nav__item_show");
+	about.classList.remove("main-nav__item_show");
+	summer.classList.remove("main-nav__item_show");
+	button.classList.remove("main-nav__item_show");
   } else {
 	fixed.classList.add("main-header__hidden");
 	fixed.classList.remove("main-header__fixed");
-	navMenu.classList.remove("main-nav-list__mobile");
-	burger.classList.remove("change");
-	gallery.classList.remove("li-show");
-	about.classList.remove("li-show");
-	summer.classList.remove("li-show");
-	button.classList.remove("li-show");
+	navMenu.classList.remove("main-nav__list_mobile");
+	burger.classList.remove("burger_change");
+	gallery.classList.remove("main-nav__item_show");
+	about.classList.remove("main-nav__item_show");
+	summer.classList.remove("main-nav__item_show");
+	button.classList.remove("main-nav__item_show");
   }
   this.oldScroll = this.scrollY;
 }, false);
